4c45250f2fdfdcd0f7f2a17d49b30f2471a3ca16
Pedro Rendeiro <76977246+pedro-rendeiro@users.noreply.github.com>
2023-08-23 14:09:56 +0000

    Configg prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do projeto https://www.tabnews.com.br/ desenvolvido ao longo do https://curso.dev/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Hello, World!</h1>
+  return <h1>Hello, World!</h1>;
+}
+
+function Teste() {
+  return <p>Kkkkkkkkkkkk</p>;
 }
 
 export default Home;
